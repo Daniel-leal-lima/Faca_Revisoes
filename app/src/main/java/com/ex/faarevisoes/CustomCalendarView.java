ffa72be591286b32164a53e33545a5275852ba09
Daniel-leal-lima <daniel.leal.lima@hotmail.com>
2020-09-24 16:36:29 -0300

    Passo 1 - calendário customizado

---
+++ app/src/main/java/com/ex/faarevisoes/CustomCalendarView.java
@@ -0,0 +1,84 @@
+package com.ex.faarevisoes;
+
+import android.content.Context;
+import android.provider.CalendarContract;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class CustomCalendarView extends LinearLayout {
+    ImageButton nextButton,previousButton;
+    TextView currentDate;
+    GridView gridView;
+    private static final int MAX_CALENDAR_DAYS =42;
+    Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+    Context context;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy",Locale.ENGLISH);
+    SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM",Locale.ENGLISH);
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy",Locale.ENGLISH);
+
+    List<Date> dates = new ArrayList<>();
+    List<Events> eventsList = new ArrayList<>();
+
+    public CustomCalendarView(Context context) {
+        super(context);
+    }
+
+    public CustomCalendarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        InitializeLayout();
+        SetUpCalendar();
+
+        previousButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH,-1);
+                SetUpCalendar();
+            }
+        });
+
+        nextButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar.add(Calendar.MONTH,1);
+                SetUpCalendar();
+            }
+        });
+
+
+    }
+
+    public CustomCalendarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private void InitializeLayout(){
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.calendar_layout,this);
+        nextButton = view.findViewById(R.id.nextBtn);
+        previousButton = view.findViewById(R.id.previousBtn);
+        currentDate = view.findViewById(R.id.currentDate);
+        gridView = view.findViewById(R.id.gridView);
+
+    }
+
+    private void SetUpCalendar(){
+        String currentDate = dateFormat.format(calendar.getTime());
+        this.currentDate.setText(currentDate);
+    }
+    
+}
---
+++ app/src/main/java/com/ex/faarevisoes/DBOpenHelper.java
@@ -0,0 +1,66 @@
+package com.ex.faarevisoes;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.security.PublicKey;
+
+public class DBOpenHelper extends SQLiteOpenHelper {
+
+   private final static  String CREATE_EVENTS_TABLE="create table "+ DBStructure.EVENT_TABLE_NAME+"" +
+           "(ID INTEGER PRIMARY KEY AUTOINCREMENT, "+ DBStructure.EVENT+" TEXT, " + DBStructure.DATE+
+           " TEXT, " + DBStructure.MONTH + " TEXT, "+ DBStructure.YEAR + " TEXT)";
+   private static final String DROP_EVENTS_TABLE= "DROP TABLE IF EXISTS "+ DBStructure.EVENT_TABLE_NAME;
+
+    public DBOpenHelper(@Nullable Context context) {
+        super(context, DBStructure.DB_NAME, null, DBStructure.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_EVENTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DROP_EVENTS_TABLE);
+        onCreate(db);
+    }
+
+    public void SaveEvent(String event, String time, String date,
+                          String month, String year, SQLiteDatabase database){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBStructure.EVENT,event);
+        contentValues.put(DBStructure.TIME,time);
+        contentValues.put(DBStructure.DATE,date);
+        contentValues.put(DBStructure.MONTH,month);
+        contentValues.put(DBStructure.YEAR,year);
+        database.insert(DBStructure.EVENT_TABLE_NAME,null,contentValues);
+
+    }
+
+    public Cursor ReadEvents(String date, SQLiteDatabase database){
+        String[] projections = {DBStructure.EVENT,DBStructure.TIME,DBStructure.DATE,
+                                DBStructure.MONTH,DBStructure.YEAR};
+        String Selection = DBStructure.DATE+"=?";
+        String [] SelectionArgs = {date};
+
+        return database.query(DBStructure.EVENT_TABLE_NAME,projections,Selection,SelectionArgs,
+                null,null,null);
+    }
+
+    public Cursor ReadEventsPerMonths(String month, String year, SQLiteDatabase database){
+        String[] projections = {DBStructure.EVENT,DBStructure.TIME,DBStructure.DATE,
+                DBStructure.MONTH,DBStructure.YEAR};
+        String Selection = DBStructure.MONTH+"=? and" + DBStructure.YEAR+"=?";
+        String [] SelectionArgs = {month};
+
+        return database.query(DBStructure.EVENT_TABLE_NAME,projections,Selection,SelectionArgs,
+                null,null,null);
+    }
+}
